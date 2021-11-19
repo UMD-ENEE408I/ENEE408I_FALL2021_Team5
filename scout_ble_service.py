0e67da48ca431e19da23744923c2faed826ac4fa
jjtaguba <89927633+jjtaguba@users.noreply.github.com>
2021-11-19 13:00:35 -0500

    Add files via upload

--- scout_ble_service.py
+++ scout_ble_service.py
@@ -3,7 +3,11 @@ from bleak import BleakClient
 import struct
 import time
 
-address = "50:C7:CA:6F:7A:4F"
+#to import from git to jetson use:
+# $ wget github/raw/url/link/thing
+
+#address = "50:C7:CA:6F:7A:4F"
+address = "3A:A8:FE:D9:13:BE"
 MOTOR_CHAR_UUID = "0fe79935-cd39-480a-8a44-06b70f36f249"
 
 SCOUT_SERVICE_UUID = "19d10000-e8f2-537e-4f6c-d104768a1214"
